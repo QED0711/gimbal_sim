4b5d1554be5fe81e7e3579658ea86594f7e0970c
QED0711 <qdizon@gmail.com>
2023-11-17 21:16:13 -0500

    started work with rust udp sockets

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{fs::File, io::Write, path::Path, process::{Command, Stdio}, sync::Mutex};
+use std::{fs::File, io::Write, path::Path, process::{Command, Stdio}, sync::Mutex, net::UdpSocket};
 use tauri::State;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 struct AppSharedState {
-    video_stdin: Mutex<Option<std::process::ChildStdin>>,
-    data_stdin: Mutex<Option<std::process::ChildStdin>>,
+    // video_stdin: Mutex<Option<std::process::ChildStdin>>,
+    // data_stdin: Mutex<Option<std::process::ChildStdin>>,
+    video_socket: UdpSocket,
+    video_target: String,
 }
 
 #[allow(dead_code)]
@@ -21,48 +23,24 @@ fn save_image_to_disk(data: Vec<u8>){
 
 #[tauri::command]
 fn send_packet(state: State<AppSharedState>, image_arr: Vec<u8>) {
-    let mut video_stdin = state.video_stdin.lock().unwrap();
-    let mut data_stdin = state.data_stdin.lock().unwrap();
-
-    if let Some(stdin) = video_stdin.as_mut() {
-        stdin.write_all(&image_arr).expect("failed to write to video stdin");
-    }
-
-    // let dummy_data: Vec<u8> = vec![0, 1, 126, 127];
-    // if let Some(stdin) = data_stdin.as_mut() {
-    //     stdin.write_all(&dummy_data).expect("failed to write to data stdin");
-    // }
+    let _ = state.video_socket.send_to(&image_arr, state.video_target.as_str());
 }
 
 fn main() {
 
-    let video_command = "-f image2pipe -c:v mjpeg -i - -f mpegts udp://127.0.0.1:8888"; // video only
-    let data_command = "-f data -i - -c copy -f mpegts udp://127.0.0.1:8889"; // video & data
-    let multicast_command = "-i udp://127.0.0.1:8888 -i udp://127.0.0.1:8889 -map 0 -map 1 -c copy -f mpegts udp://239.0.0.1:8000";
+    let video_socket = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind to video socket");
+    let video_target = "239.0.0.2:8888".to_string();
 
-    let ffmpeg_video = Command::new("ffmpeg")
-        .args(video_command.split(" "))
-        .stdin(Stdio::piped())
+    let ffmpeg_command = "-c:v mjpeg -i udp://239.0.0.2:8888 -map 0:v -c copy -f mpegts udp://239.0.0.1:8000";
+
+    let _ = Command::new("ffmpeg")
+        .args(ffmpeg_command.split(" "))
         .spawn()
-        .expect("Failed to start FFmpeg Video Stream");
-
-    let ffmpeg_data = Command::new("ffmpeg")
-        .args(data_command.split(" "))
-        .stdin(Stdio::piped())
-        .spawn()
-        .expect("Failed to start FFmpeg Data Stream");
-
-    let ffmpeg_multicast = Command::new("ffmpeg")
-        .args(multicast_command.split(" "))
-        .spawn()
-        .expect("Failed to start FFmpeg Multicast Stream");
-
-    let video_stdin = Mutex::new(ffmpeg_video.stdin);
-    let data_stdin = Mutex::new(ffmpeg_data.stdin);
+        .expect("Failed to start ffmpeg multicast process");
 
 
     tauri::Builder::default()
-        .manage(AppSharedState {video_stdin, data_stdin})
+        .manage(AppSharedState {video_socket, video_target})
         .invoke_handler(tauri::generate_handler![
             send_packet
         ])
